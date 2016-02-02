bfdb7c70bbbf0f8285d88846c0bb5af73d561d5a
Michael Sadowski <msadowsk@hulk.socs.uoguelph.ca>
2016-02-02 13:59:03 -0500

    CIS*3260 - Software Design IV

---
+++ CIS*3260/A2/Server.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright &#169; 2004, 2010 Oracle and/or its affiliates. All  Rights Reserved.
+ *  
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the following 
+ * conditions are met:
+ * 
+ * -Redistributions of source code must retain the above copyright  
+ *  notice, this list of conditions and the following disclaimer.
+ * 
+ * -Redistribution in binary form must reproduce the above copyright 
+ *  notice, this list of conditions and the following disclaimer in 
+ *  the documentation and/or other materials provided with the 
+ *  distribution.
+ *  
+ * Neither the name of Oracle and/or its affiliates. or the names of 
+ * contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission.
+ * 
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. Oracle and/or its affiliates. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,
+ * SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
+ * THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *  
+ * You acknowledge that Software is not designed, licensed or 
+ * intended for use in the design, construction, operation or 
+ * maintenance of any nuclear facility.
+ */
+package example.hello;
+	
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+
+	
+public class Server implements MyGuru {
+	
+	
+	
+    public Server() 
+    {
+    
+    
+    }
+
+    public String whatIsTheAnswer(String question) {
+	
+	Scanner in = new Scanner(System.in);
+	System.out.println("Question: " + question);
+	System.out.printf("Answer: ");
+	String answer = in.nextLine();
+	return answer;
+    }
+	
+    public static void main(String args[]) {
+	
+	try {
+	    Server obj = new Server();
+	    MyGuru stub = (MyGuru) UnicastRemoteObject.exportObject(obj, 0);
+
+	    // Bind the remote object's stub in the registry
+	    Registry registry = LocateRegistry.getRegistry();
+	    registry.bind("Hello", stub);
+
+	    System.err.println("Server ready");
+	} catch (Exception e) {
+	    System.err.println("Server exception: " + e.toString());
+	    e.printStackTrace();
+	}
+    }
+}
